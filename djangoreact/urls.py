263e517004df36938b430d8802d4fc80067fadf5
Willy Claes <willy.claes821@gmail.coom>
2017-02-27 20:53:38 +0100

    Fix to use react-router for all unmatched routes.

--- djangoreact/urls.py
+++ djangoreact/urls.py
@@ -8,9 +8,9 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    url(r'^$', views.index),
     url(r'^api/auth/', include('rest_auth.urls')),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', views.index),
 ]
