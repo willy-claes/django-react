3a9a40280557d9ad96bca969d735d591543d5392
Willy Claes <willy.claes821@gmail.coom>
2017-02-27 17:54:19 +0100

    Update server package to add default config.

--- djangoreact/settings.py
+++ djangoreact/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'server.apps.ServerConfig',
+    'server',
     'webpack_loader',
     'rest_framework',
     'django.contrib.admin',
