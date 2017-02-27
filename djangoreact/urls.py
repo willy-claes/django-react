99dd677271fcc46fb11bd9923228e9ccc414eaa7
Willy Claes <willy.claes821@gmail.coom>
2017-02-27 10:49:02 +0100

    Rename package name from app to server.

--- djangoreact/urls.py
+++ djangoreact/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
-from app import views
+from server import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
